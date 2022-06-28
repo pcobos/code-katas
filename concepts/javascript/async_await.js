5688f466c5bae2757cd6bb3595932d09c8f55972
Pedro Cobos <pedro.cobos@live.com>
2022-06-27 21:20:17 -0500

    Async await refactored

--- concepts/javascript/async_await.js
+++ concepts/javascript/async_await.js
@@ -9,13 +9,24 @@ let setTimeoutPromise = (delay) => {
 };
 
 // Following code snipped is how things would be done with a promise
-setTimeoutPromise(250)
-  .then((msg) => {
-    console.log(msg);
-    console.log("First timeout");
-    return setTimeoutPromise(500);
-  })
-  .then((msg) => {
-    console.log(msg);
-    console.log("Second timeout");
-  });
+// setTimeoutPromise(250)
+//   .then((msg) => {
+//     console.log(msg);
+//     console.log("First timeout");
+//     return setTimeoutPromise(500);
+//   })
+//   .then((msg) => {
+//     console.log(msg);
+//     console.log("Second timeout");
+//   });
+let doSomething = async () => {
+  let msg1 = await setTimeoutPromise(250);
+  console.log(msg1);
+  console.log("First timeout");
+
+  let msg2 = await setTimeoutPromise(500);
+  console.log(msg2);
+  console.log("First timeout");
+};
+
+doSomething();
